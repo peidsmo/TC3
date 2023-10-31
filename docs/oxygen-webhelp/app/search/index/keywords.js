101af73cc52e118f370a08487b9e2ed46d2f38cc
peidsmo <145617780+peidsmo@users.noreply.github.com>
2023-10-31 17:05:54 -0500

    Add files via upload

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,16 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Send",p:["p1"]},{w:"Messages",p:["p1"]},{w:"to",p:["p1","p6","p8"]},{w:"Students",p:["p1","p4","p8"]},{w:"Announcements",p:["p2"]},{w:"Calendar",p:["p3"]},{w:"communications",p:["p3"]},{w:"Canvas:",p:["p4"]},{w:"Communicate",p:["p4"]},{w:"with",p:["p4"]},{w:"Canvas",p:["p5"]},{w:"communication",p:["p5"]},{w:"tools",p:["p5"]},{w:"How",p:["p6"]},{w:"use",p:["p6","p9"]},{w:"the",p:["p6","p7","p8","p9"]},{w:"Inbox",p:["p6","p7","p9"]},{w:"as",p:["p6"]},{w:"an",p:["p6"]},{w:"instructor",p:["p6"]},{w:"Learn",p:["p7"]},{w:"about",p:["p7"]},{w:"Message",p:["p8"]},{w:"from",p:["p8"]},{w:"gradebook",p:["p8"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6];
+ph["p2"]=[7];
+ph["p3"]=[8, 9];
+ph["p4"]=[10, 11, 12, 6];
+ph["p5"]=[13, 14, 15];
+ph["p6"]=[16, 5, 17, 18, 19, 20, 21, 22];
+ph["p7"]=[23, 24, 18, 19];
+ph["p8"]=[25, 5, 6, 26, 18, 27];
+ph["p9"]=[17, 18, 19];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {6:9,8:-1,4:-1,9:4,7:4,5:-1,3:-1,2:-1,1:4};
